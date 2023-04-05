16e96a8e6c72ef2e4f10931f612bc3f61e2f5976
josephleung5 <joseph.leung5@gmail.com>
2023-04-05 01:16:12 +0100

    feat(CommonCard): create CommonCard component

---
+++ components/common/CommonCard/CommonCard.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import type { Launch } from '../../../types/types';
+import CommonCard from "./CommonCard";
+
+const failLaunch: Launch = {
+  id: '5eb87cd9ffd86e000604b32a',
+  name: 'FalconSat',
+  dateUTC: '2006-03-24T22:30:00.000Z',
+  coreName: 'Merlin1A',
+  payloads: {
+    id: '5eb0e4b5b6c3bb0006eeb1e1',
+    type: 'Satellite',
+  },
+  imageUrl: 'https://images2.imgbox.com/94/f2/NN6Ph45r_o.png',
+  isSuccess: false,
+  failureReason: 'merlin engine failure'
+}
+
+const successLaunch: Launch = {
+  id: '5eb87cd9ffd86e000604b32a',
+  name: 'FalconSat',
+  dateUTC: '2006-03-24T22:30:00.000Z',
+  coreName: 'Merlin1A',
+  payloads: {
+    id: '5eb0e4b5b6c3bb0006eeb1e1',
+    type: 'Satellite',
+  },
+  imageUrl: 'https://images2.imgbox.com/94/f2/NN6Ph45r_o.png',
+  isSuccess: true,
+  failureReason: 'merlin engine failure'
+}
+
+describe('CommonCard', () => {
+  it('renders success', () => {
+    render(<CommonCard launch={successLaunch} handleModalOpen={() => console.log('open')} />)
+
+    const successText = screen.getByText('SUCCESS');
+
+    expect(successText).toBeTruthy();
+  })
+
+  it('renders fail with reason', () => {
+    render(<CommonCard launch={failLaunch} handleModalOpen={() => console.log('open')} />)
+
+    const failText = screen.getByText('FAIL');
+
+    expect(failText).toBeTruthy();
+  })
+})
---
+++ components/common/CommonCard/CommonCard.tsx
@@ -0,0 +1,37 @@
+
+import type { Launch } from "../../../types/types"
+import {
+  Card,
+  Image,
+  Text,
+  Title,
+} from "@mantine/core";
+
+export type CommonCardProps = {
+  launch: Launch;
+  handleModalOpen: (launch: Launch) => void;
+}
+
+const CommonCard = ({
+  launch,
+  handleModalOpen,
+}: CommonCardProps) => {
+  return (
+    <Card 
+      onClick={() => handleModalOpen(launch)}
+      sx={{ cursor: 'pointer' }}
+    >
+      <Card.Section>
+        <Image fit='contain' height={350} src={launch.imageUrl} alt={launch.name} />
+      </Card.Section>
+      <Title order={3}>{launch.name}</Title>
+      <Text>{launch.dateUTC}</Text>
+      <Text>{launch.coreName}</Text>
+      <Text color={launch.isSuccess ? 'green' : 'red'}>
+        {launch.isSuccess ? 'SUCCESS' : 'FAIL'}
+      </Text>
+    </Card>
+  )
+}
+
+export default CommonCard;
